9e83dd1bec31b4b9c3db6c9ba0431b7729f3f7db
KshitijaTambeGitHub <kshitijatambe98@gmail.com>
2023-03-16 23:15:08 +0530

    ArrayList Added

---
+++ Collection/src/com/demo/github/ArrayListDemo.java
@@ -0,0 +1,12 @@
+package com.demo.github;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		List list =Arrays.asList("Kshitija","Sonali", "Prajkta");
+		System.out.println(list);
+	}
+
+}
